5c74ec0b79a1f4223f26dcd08a123e2479110537
Pooja Sampatrao Ingale <90215174+ingalepooja@users.noreply.github.com>
2025-03-25 17:12:16 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,25 @@
+"""Variables
+A variable is a name given to a memory location in a program. 
+
+name = "Pooja" --> name = variable and Pooja = value
+* Identifiers = class/function/variable name
+* Rules for Identifiers
+   1. It can be combination of uppercase & lowercase letters, digits, or an underscore.
+   2. It can't start with digit.
+   3. We can't use special symbols like !, @, #, $, etc.
+   4. It can be of any length.
+
+"""
+name = "v-Uptown"
+floor = 5
+rent = 13.5
+
+print(name)
+print(floor)
+print(rent)
+
+print("I lived in a" , name, "Society.")
+print("My room was on the" , floor,"th", "floor.")
+print("Rent is" , rent,"k.")
+
+print("I lived in a" , name, "Society.", "My room was on the" , floor,"th", "floor.", "Rent is" , rent,"k." )
