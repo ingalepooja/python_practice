d9bded5021928b09ce58d244c7c4c0d1e7d638da
Pooja Sampatrao Ingale <90215174+ingalepooja@users.noreply.github.com>
2025-03-25 16:32:43 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,13 @@
+name = "v-Uptown"
+floor = 5
+rent = 13.5
+
+print(name)
+print(floor)
+print(rent)
+
+print("I lived in a" , name, "Society.")
+print("My room was on the" , floor,"th", "floor.")
+print("Rent is" , rent,"k.")
+
+print("I lived in a" , name, "Society.", "My room was on the" , floor,"th", "floor.", "Rent is" , rent,"k." )
